278d6cd4167ac5c499fab14375f6383dcaf1b192
Nilesh Mishra <thenileshmishra@gmail.com>
2025-05-04 12:02:49 +0530

    modes user.js is added succesfully

---
+++ backend/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // Database configuration
+    db:{
+        url: 'mongodb://localhost:27017/AdminLens',
+        options:{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    }
+  };
---
+++ backend/model/user.js
@@ -0,0 +1,26 @@
+const mongoose = reuquire('mongoose');
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: true,
+    },  
+    
+})
