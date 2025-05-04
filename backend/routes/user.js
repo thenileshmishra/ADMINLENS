00ff460ead6e1feb43539694b125672006b3376e
Nilesh Mishra <thenileshmishra@gmail.com>
2025-05-04 12:39:26 +0530

    User routes is already updated

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,13 +2,32 @@ const express = require("express");
 const router = express.Router();
 const User = require('../models/user');
 
+
+   // GET all users
+
 router.get('/', async (req, res)=>{
     try{
-        const users = await User.find().limit(10).skip(10);
+        const users = await User.find();
         res.json(users);
     }catch(error){
         res.status(500).json({message: error.message});
     }
 });
 
+//POST a new user
+
+router.post('/', async (req, res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+    })
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch(err){
+        res.status(400).json({ message: err.message });
+    }
+
+})
 
